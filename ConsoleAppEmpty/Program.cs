5cdec28995b8de7a2ed040aad3bd93ed230f4a1d
austen.hernandez <austenh05@gmail.com>
2026-05-15 14:21:06 -0600

    created code for part one

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -1,4 +1,7 @@
-﻿public class Program
+﻿// This pogram is for the Intro to Github assignment for CS 1415
+// Austen Hernandez
+// 5/15/26
+public class Program
 {
     static void Main(string[] args)
     {
